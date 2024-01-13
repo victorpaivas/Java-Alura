769b97dd558934743713e51e06003da8820b27f2
Victor Paiva <129319485+victorpaivas@users.noreply.github.com>
2024-01-12 21:30:18 -0300

    Aula 01 com desafios

--- desafios/src/ConversorDeDolarEmReais.java
+++ desafios/src/ConversorDeDolarEmReais.java
@@ -5,6 +5,6 @@ public class ConversorDeDolarEmReais {
         double valorEmDolares = 53.30;
         double cambioDeDolarParaReal = (valorEmDolares * 4.85);
 
-        System.out.println(String.format("O valor de %.2f dolares equivale a %.2f reais.", valorEmDolares, cambioDeDolarParaReal));
+        System.out.println(String.format("O valor de %.2f dolares equivale a %.2f reais..", valorEmDolares, cambioDeDolarParaReal));
     }
 }
